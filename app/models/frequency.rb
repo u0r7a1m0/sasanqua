59007e54835c73160c6d28b0488ca86d8135e3cd
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2023-01-15 09:16:20 +0000

    [Update]enum追記

--- app/models/frequency.rb
+++ app/models/frequency.rb
@@ -1,4 +1,11 @@
 class Frequency < ApplicationRecord
   belongs_to :routine
   validates :frequency, presence: true
+  enum frequency:{
+    oneday_once:  0, #1日に1回
+    oneday_twice: 1, #1日に2回
+    oneday_third: 2, #1日に3回
+    twoday_once: 3, #2日に1回
+    threeday_once: 4 #3日に1回
+  }
 end
--- app/models/period.rb
+++ app/models/period.rb
@@ -1,4 +1,13 @@
 class Period < ApplicationRecord
   belongs_to :routine
   validates :period, presence: true
+  enum priod:{
+    everyday: 0, #ずっと続ける
+    one_week: 1, #1週間
+    two_week: 2, #2週間
+    three_week: 3, #3週間
+    four_week: 4, #4週間
+    one_month: 5, #1ヶ月
+    one_year: 6 #1年間
+  }
 end
