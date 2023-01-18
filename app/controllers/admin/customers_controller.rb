d46b49c510b95ad473620bf21d72f1b9af8cb875
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2023-01-18 15:02:27 +0000

    [Update]css更新

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,4 +1,7 @@
 class Admin::CustomersController < ApplicationController
+  def index
+    @customers = Customer.all.order("created_at DESC")
+  end
   def show
     @customer = Customer.find(params[:id])
     @routines = @customer.routines
--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,6 +2,7 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
   def top
     @customers = Customer.all.order("created_at DESC")
+    @routines = Routine.all.order("created_at DESC")
   end
   def customer_params
     params.require(:customer).permit(:customer_id, :profile_image, :nickname, :email, :is_deleted, :created_at, :updated_at)
